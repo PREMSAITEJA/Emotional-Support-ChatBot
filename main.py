aaabfad479703d088ce8b503524acdd91a5ac42a
PREMSAITEJA <premsaiteja58@gmail.com>
2025-02-01 21:05:04 +0530

    final commit

--- main.py
+++ main.py
@@ -125,7 +125,9 @@ def main():
     st.sidebar.success(
         "### Disclaimer\n"
         "This chatbot is for informational purposes only and is not a substitute for professional medical advice, diagnosis, or treatment. "
-        "Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition."
+        "Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition.\n\n"
+        "Patents: - THULVA PRAM SAI TEJA \n\n"
+        "Team Name : premsaiteja58_c67e"
     )
 
 if __name__ == "__main__":
--- virtual/Scripts/activate.bat
+++ virtual/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\opencv\Emotional-Support-ChatBot\virtual
+set VIRTUAL_ENV=C:\opencv\Amdocs-Projects\Emotional-Support-ChatBot\virtual
 
 if not defined PROMPT set PROMPT=$P$G
 
